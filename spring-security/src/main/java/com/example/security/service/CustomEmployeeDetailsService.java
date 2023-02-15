e3b14ad59330082572ca2a734d1dc3b999d3f2b2
eungsu <eungsu@me.com>
2023-02-15 17:53:57 +0900

    modify securityConfig

--- spring-security/src/main/java/com/example/security/service/CustomEmployeeDetailsService.java
+++ spring-security/src/main/java/com/example/security/service/CustomEmployeeDetailsService.java
@@ -1,11 +1,9 @@
 package com.example.security.service;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
