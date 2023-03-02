ea0e66a069a1688147f8f972dabd5f8a25436f5c
eungsu <eungsu@me.com>
2023-03-02 15:58:45 +0900

    카카오 사용자 정보 조회 수정

--- oauth2-social-login-sample/src/main/java/com/example/security/oauth/info/KakaoOAuth2UserInfo.java
+++ oauth2-social-login-sample/src/main/java/com/example/security/oauth/info/KakaoOAuth2UserInfo.java
@@ -6,7 +6,6 @@ public class KakaoOAuth2UserInfo extends OAuth2UserInfo {
 
 	public KakaoOAuth2UserInfo(Map<String, Object> attributes) {
 		super(attributes);
-		System.out.println(attributes);
 	}
 	
 	@Override
